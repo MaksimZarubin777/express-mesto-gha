ba692c6a7eab675c8f48bab1aef3d9bee582c965
Maksim Zarubin <89940028777@ya.ru>
2023-04-23 21:47:16 +0400

    express_app-fixed

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.use((req, res, next) => {
 });
 app.use(userRouter);
 app.use(cardRouter);
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
